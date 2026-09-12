c8a4d62a642c95bd81593a5a9cb7b417025a4b3f
ivoge <ivoge@IVOSPC>
2026-09-12 18:52:50 +0300

    Add project files.

---
+++ IntelliLoop.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using IntelliLoop.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IntelliLoop.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IntelliLoop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntelliLoop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
